bfb6986987215bcea8d4b2b21e2db8957f16963b
gaurav-grip <gaurav.jaiman@gripinvest.in>
2025-05-19 07:20:17 +0530

    removed .env

--- src/components/uploadbutton.tsx
+++ src/components/uploadbutton.tsx
@@ -13,7 +13,10 @@ const UploadButton = ({ onUploadSuccess }: UploadButtonProps) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await fetch("/api/upload/csv", {
+    const API_BASE_URL =
+      import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+
+    const res = await fetch(`${API_BASE_URL}/upload/csv`, {
       method: "POST",
       body: formData,
     });
